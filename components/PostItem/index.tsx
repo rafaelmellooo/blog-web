a6e6ef7cb6759889ed1a2b503d94ffab7b93f845
Rafael Mello <rafaelmello0715@outlook.com>
2020-06-12 05:05:14 -0300

    feat: add PostItem component

---
+++ components/PostItem/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+const PostItem: React.FC = () => (
+  <Container>
+    <Title>Dólar fecha em alta, mas abaixo de R$ 4,90</Title>
+  </Container>
+);
+
+export default PostItem;
---
+++ components/PostItem/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.article`
+  min-height: 300px;
+  background-color: #ffb997;
+  border-radius: 4px;
+  padding: 16px;
+  display: flex;
+  align-items: flex-end;
+`;
+
+export const Title = styled.h2``;
