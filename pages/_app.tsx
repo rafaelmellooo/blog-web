a1410f405f48f2d65be2d854da8d4a4f4eaaeac7
Rafael Mello <rafaelmello0715@outlook.com>
2020-06-13 07:05:13 -0300

    fix: problem in Custom App and fixed with Class Component that extends NextApp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,31 @@
-import { NextPage } from 'next';
 import Head from 'next/head';
-import { AppProps } from 'next/app';
+import NextApp from 'next/app';
 import styled from 'styled-components';
 
 import { GlobalStyle } from '../styles/global';
 
-const App: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@500;700&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <GlobalStyle />
+class App extends NextApp {
+  render(): JSX.Element {
+    const { Component, pageProps } = this.props;
 
-    <Container>
-      <Component {...pageProps} />
-    </Container>
-  </>
-);
+    return (
+      <>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <GlobalStyle />
+
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </>
+    );
+  }
+}
 
 const Container = styled.main`
   min-height: 100vh;
