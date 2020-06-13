4f562a0cf5039ef7b0aebfd77e5f0404ba462aa6
Rafael Mello <rafaelmello0715@outlook.com>
2020-06-13 07:08:29 -0300

    feat: increment index page with server side props and PostItem components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
-import { NextPage } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
+import { FiPlus } from 'react-icons/fi';
 import Head from 'next/head';
+import Link from 'next/link';
+import api from '../services/api';
 import styled from 'styled-components';
 
 import PostItem from '../components/PostItem';
 
-const Home: NextPage = () => {
+type Post = {
+  id: number;
+  title: string;
+  slug: string;
+  user: {
+    name: string;
+  };
+  image_url: string;
+};
+
+type HomeProps = {
+  posts: Post[];
+};
+
+const Home: NextPage<HomeProps> = ({ posts }) => {
   return (
     <>
       <Head>
@@ -12,12 +29,25 @@ const Home: NextPage = () => {
       </Head>
 
       <Container>
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
+        <Link href="/posts/new">
+          <a>
+            <AddPost>
+              <FiPlus />
+            </AddPost>
+          </a>
+        </Link>
+
+        {posts.map((post) => (
+          <Link key={post.id} href="/posts/[slug]" as={`/posts/${post.slug}`}>
+            <a>
+              <PostItem
+                title={post.title}
+                image_url={post.image_url}
+                author={post.user.name}
+              />
+            </a>
+          </Link>
+        ))}
       </Container>
     </>
   );
@@ -29,4 +59,29 @@ const Container = styled.section`
   grid-gap: 24px;
 `;
 
+const AddPost = styled.article`
+  background-color: #0b032d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+
+  svg {
+    width: 50%;
+    height: 50%;
+    color: #fff;
+  }
+`;
+
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await api.get<Post[]>('/posts');
+
+  return {
+    props: {
+      posts: response.data,
+    },
+  };
+};
